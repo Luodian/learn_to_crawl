fdf6cd1acd70c2a3d399202bf72c5969767de576
Luodian <1121058986@qq.com>
2017-05-17 00:13:05 +0800

    ch1.4.4

---
+++ learn_to_download/chapter_1.md
@@ -0,0 +1,7 @@
+#Chapter 1
+
+1. [re.findall作用]("http://blog.csdn.net/cashey1991/article/details/8875213")
+2. [sitemap作用]("https://zh.wikipedia.org/wiki/%E7%B6%B2%E7%AB%99%E5%9C%B0%E5%9C%96")
+3. [sitemap详细说明](https://www.sitemaps.org/protocol.html#escaping "https://www.sitemaps.org/protocol.html#escaping")
+
+
--- learn_to_download/main.py
+++ learn_to_download/main.py
@@ -1,7 +1,8 @@
 import urllib2
 import whois
 import builtwith
-import os
+import itertools
+import re
 
 
 def getwho(url):
@@ -14,20 +15,49 @@ def getbuiltwith(url):
 
 def download(url, user_agent = 'wswp', num_retries = 2):
     print "Downloading: ", url
-    headers = ["User-agent", user_agent]
+    headers = {"User-agent": user_agent}
     request = urllib2.Request(url, headers = headers)
     try:
-        html = urllib2.urlopen(url).read()
+        html = urllib2.urlopen(request).read()
     except urllib2.URLError as e:
         print "Download error: ", e.reason
         html = None
         if num_retries > 0:
             if hasattr(e, 'code') and 500 <= e.code < 600:
-                return download(url, num_retries - 1)
+                return download(url, user_agent, num_retries - 1)
     return html
 
+def crawl_sitemap(url):
+    # download the sitemap file
+    sitemap = download(url)
+    # extract the sitemap links
+    links = re.findall('<loc>(.*?)</loc>',sitemap)
+    # download each link
+    for link in links:
+        html = download(link)
+
+
+# 遍历ID的方式进行sitemap的爬取
+# maximum number of consecutive download errors allowed
+max_errors = 5
+# current number of consecutive download errors allowed
+num_errors = 0
+
+def easy_download():
+    for page in itertools.count(1):
+        url = 'http://example.webscraping.com/view/-%d', % page
+        html = None
+        if hstml is None:
+            num_errors ++
+            if (num_errors >= max_errors):
+                break
+        else:
+            num_errors = 0
+
+
 if __name__ == "__main__":
-    html = download("http://www.meetup.com/")
-    file = open("/Users/luodian/Desktop/test.html","w")
-    file.writelines(html)
+    # html = download("http://www.meetup.com/")
+    # file = open("/Users/luodian/Desktop/test.html", "w")
+    # file.writelines(html)
+    crawl_sitemap('http://example.webscraping.com/sitemap.xml')
 
